8178fd2b4c8198401cb492849bb2db3abb891809
chengbo.gao <chengbo.gao@thundersoft.com>
2023-02-06 17:16:06 +0800

    add noice

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -270,7 +270,21 @@ plugins = {
             {"nvim-lua/plenary.nvim"},
             {"neovim/nvim-lspconfig"}
         }
-    }
+    },
+    {
+        "folke/noice.nvim",
+        config = function()
+          require "interface.noice"
+        end,
+        dependencies = {
+          -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+          "MunifTanjim/nui.nvim",
+          -- OPTIONAL:
+          --   `nvim-notify` is only needed, if you want to use the notification view.
+          --   If not available, we use `mini` as the fallback
+          "rcarriga/nvim-notify",
+        },
+      },
 }
 
 options = {
