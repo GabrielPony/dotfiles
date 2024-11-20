836081ac709296ad88a22bcf4d8aaf790eb7167d
GabrielGao <56246210+GabrielPony@users.noreply.github.com>
2024-11-20 10:22:27 +0800

    Update keymaps.lua

--- nvim/custom/config/keymaps.lua
+++ nvim/custom/config/keymaps.lua
@@ -1,4 +1,4 @@
-local map = LazyVim.safe_keymap_set
+local map = vim.keymap.set
 
 map({"i", "v"}, "jk", "<ESC>", {desc = "ESC"})
 
@@ -11,20 +11,18 @@ map({ "n", "x" }, "<Down>", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr =
 map({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
 map({ "n", "x" }, "<Up>", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
 
--- 调整分割窗口大小
+
 map("n", "<A-h>", require('smart-splits').resize_left, { desc = "Resize left" })
 map("n", "<A-j>", require('smart-splits').resize_down, { desc = "Resize down" })
 map("n", "<A-k>", require('smart-splits').resize_up, { desc = "Resize up" })
 map("n", "<A-l>", require('smart-splits').resize_right, { desc = "Resize right" })
 
--- 在分割窗口间移动光标
 map("n", "<C-h>", require('smart-splits').move_cursor_left, { desc = "Move cursor left" })
 map("n", "<C-j>", require('smart-splits').move_cursor_down, { desc = "Move cursor down" })
 map("n", "<C-k>", require('smart-splits').move_cursor_up, { desc = "Move cursor up" })
 map("n", "<C-l>", require('smart-splits').move_cursor_right, { desc = "Move cursor right" })
 map("n", "<C-\\>", require('smart-splits').move_cursor_previous, { desc = "Move cursor previous" })
 
--- 在窗口间交换缓冲区
 map("n", "<leader><leader>h", require('smart-splits').swap_buf_left, { desc = "Swap buffer left" })
 map("n", "<leader><leader>j", require('smart-splits').swap_buf_down, { desc = "Swap buffer down" })
 map("n", "<leader><leader>k", require('smart-splits').swap_buf_up, { desc = "Swap buffer up" })
