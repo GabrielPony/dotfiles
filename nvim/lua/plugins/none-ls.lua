2d106a86fd51893c4b70373428df2af5f0477140
GabrielPony <aoengo@outlook.com>
2024-11-23 05:04:20 +0000

    update nvim

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -1,23 +1,53 @@
--- Customize None-ls sources
-
----@type LazySpec
 return {
   "nvimtools/none-ls.nvim",
-  opts = function(_, opts)
-    -- opts variable is the default configuration table for the setup function call
-    local null_ls = require "null-ls"
+  opts = function(_, config)
+    local null_ls = require("null-ls")
 
-    -- Check supported formatters and linters
-    -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/formatting
-    -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
+    config.sources = {
+      null_ls.builtins.formatting.nixpkgs_fmt,
 
-    -- Only insert new sources, do not replace the existing ones
-    -- (If you wish to replace, use `opts.sources = {}` instead of the `list_insert_unique` function)
-    opts.sources = require("astrocore").list_insert_unique(opts.sources, {
-      -- Set a formatter
-      null_ls.builtins.formatting.stylua,
-      null_ls.builtins.formatting.prettier,
-      null_ls.builtins.formatting.clang_format,
-    })
+      null_ls.builtins.formatting.stylua.with({
+        extra_args = { "--print-width=120", "--indent-type=Spaces", "--indent-width=4" },
+      }),
+
+      null_ls.builtins.formatting.black.with({
+        extra_args = {
+          "--tab-width=4",
+          "--print-width=120",
+        },
+      }),
+
+      null_ls.builtins.formatting.clang_format.with({
+        extra_args = {
+          "--style={BasedOnStyle: LLVM, IndentWidth: 4, ColumnLimit: 120, \
+                    AllowShortFunctionsOnASingleLine: Empty, \
+                    AllowShortIfStatementsOnASingleLine: false, \
+                    AllowShortLoopsOnASingleLine: false, \
+                    SortIncludes: true, \
+                    BreakBeforeBraces: Attach}",
+        },
+        filetypes = { "c", "cpp", "objc", "objcpp" },
+      }),
+
+      null_ls.builtins.formatting.prettier.with({
+        extra_args = { "--tab-width=4", "--print-width=120" },
+      }),
+    }
+
+    local clang_format_files = {
+      ".clang-format",
+      "_clang-format",
+    }
+
+    local has_clang_format_config = function(utils)
+      return utils.root_has_file(clang_format_files)
+    end
+
+    table.insert(config.sources, null_ls.builtins.formatting.clang_format.with({
+      condition = has_clang_format_config,
+    }))
+
+    return config
   end,
 }
+
