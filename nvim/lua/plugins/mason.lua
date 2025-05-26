b548985853480425e2739d3b19907a74f26d22df
GabrielGao <56246210+GabrielPony@users.noreply.github.com>
2025-05-26 10:04:41 +0000

    update nvim config

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -8,69 +8,13 @@ return {
     opts = {
       -- Make sure to use the names found in `:Mason`
       ensure_installed = {
-        -- install language servers
         "lua-language-server",
-
-        -- install formatters
-        "stylua",
-
-        -- install debuggers
-        -- "debugpy",
-
-        -- install any other package
-        "tree-sitter-cli",
-      },
-    },
-  },
-  -- use mason-lspconfig to configure LSP installations
-  {
-    "williamboman/mason-lspconfig.nvim",
-    -- overrides `require("mason-lspconfig").setup(...)`
-    opts = {
-      ensure_installed = {
-        "nil_ls",
-        "lua_ls",
-        "pyright",
         "clangd",
-        -- add more arguments for adding more language servers
-      },
-    },
-  },
-  -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
-  {
-    "jay-babu/mason-null-ls.nvim",
-    -- overrides `require("mason-null-ls").setup(...)`
-    opts = {
-      ensure_installed = {
-        "nixpkgs-fmt",
         "stylua",
-        "black",
-        "clang-format",
-        "prettier",
-        -- add more arguments for adding more null-ls sources
+        "pyright",   -- need python3 and pip3 to be supported
+        "tree-sitter-cli",
+        "clang-format",  -- install sudo apt install -y python3-venv to solve install failed issue
       },
     },
-  },
-  {
-    "jay-babu/mason-nvim-dap.nvim",
-    -- overrides `require("mason-nvim-dap").setup(...)`
-    opts = {
-      ensure_installed = {
-        "python",
-        "cpptools",
-        -- add more arguments for adding more debuggers
-      },
-    },
-  },
-  {
-    "williamboman/mason.nvim",
-    opts = function(_, opts)
-      -- Where Mason should put its bin location in your PATH. Can be one of:
-      -- - "prepend" (default, Mason's bin location is put first in PATH)
-      -- - "append" (Mason's bin location is put at the end of PATH)
-      -- - "skip" (doesn't modify PATH)
-      ---@type '"prepend"' | '"append"' | '"skip"'
-      opts.PATH = "append" -- use mason's package only when no other package is found
-    end,
   },
 }
