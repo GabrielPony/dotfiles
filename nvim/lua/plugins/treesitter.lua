96f03298ede0abad4802bc678d62c044b8a2fbf8
Gabriel <aoengo@outlook.com>
2025-12-20 09:57:03 +0800

    update ts

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,7 +1,8 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  main = "nvim-treesitter.configs",
+
   opts = {
-    auto_install = false,
     ensure_installed = {
       "lua",
       "vim",
@@ -11,6 +12,19 @@ return {
       "python",
       "markdown",
       -- add more arguments for adding more treesitter parsers
+    },
+    highlight = { enable = true },
+    indent = { enable = true },
+    -- 如果有 textobjects 配置，也写在这里
+    textobjects = {
+      select = {
+        enable = true,
+        lookahead = true,
+        keymaps = {
+          ["af"] = "@function.outer",
+          ["if"] = "@function.inner",
+        },
+      },
     },
   },
 }
